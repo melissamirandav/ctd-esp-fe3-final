ff93d9977c8f72f8c3a5db308313ed37c3d72a06
melissamirandav <memiv21@gmail.com>
2023-11-27 15:33:38 -0500

    agregando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import Detail from "./Routes/Detail";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
 
 function App() {
   return (
       <div className="App">
           <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/dentist/:id" element={<Detail/>}/>
+            <Route path="/contacto" element={<Contact/>}/>
+            <Route path="/favs" element={<Favs/>}/> 
+          </Routes>
           <Footer/>
       </div>
   );
