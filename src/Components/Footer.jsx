7df0cce26f6f06313a0c58d2641dbca5f7a141be
melissamirandav <memiv21@gmail.com>
2023-11-30 15:31:44 -0500

    ajustando footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,7 +7,14 @@ const Footer = () => {
   return (
     <footer className={state.theme}>
         <p>Powered by</p>
-        <img src="./images/DH.png" alt='DH-logo' />
+        <img src="/images/DH.png" alt='DH-logo' />
+
+        <div className='redes-sociales'>
+          <img src="/images/ico-facebook.png" alt="logo-facebook" width={25} height={25}/>
+          <img src="/images/ico-instagram.png" alt="logo-instagram" width={25} height={25} />
+          <img src="/images/ico-tiktok.png" alt="logo-tiktok" width={25} height={25}/>
+          <img src="/images/ico-whatsapp.png" alt="logo-whatsapp" width={25} height={25} />
+        </div>
     </footer>
   )
 }
